49b8dd60dc364c13eaad284c9db545b572645101
Rob DeRosa <rob-derosa@github.com>
2020-07-26 16:17:40 -0600

    based on issues now

--- dist/index.js
+++ dist/index.js
@@ -525,8 +525,23 @@ function run() {
             const subKey = core.getInput("azureCognitiveSubscriptionKey", { required: true });
             const url = core.getInput("azureCognitiveEndpoint", { required: true });
             const gitHubToken = core.getInput("gitHubToken", { required: true });
-            console.log(github.context.payload);
-            console.log(JSON.stringify(github.context.payload));
+            console.log(`PAYLOAD: ${github.context.payload}`);
+            console.log(`STRINGIFIED: ${JSON.stringify(github.context.payload)}`);
+            const client = github.getOctokit(gitHubToken);
+            let textToCheck;
+            if (github.context.payload.issue && (github.context.payload.action == 'opened' || github.context.payload.action == 'updated')) {
+                //An issue was opened or updated
+                const { data: issue } = yield client.issues.get({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    issue_number: github.context.issue.number
+                });
+                textToCheck = issue.body;
+            }
+            if (github.context.payload.comment && github.context.payload.action == 'created') {
+                //A comment was added to the issue
+                textToCheck = github.context.payload.comment.body;
+            }
             if (github.context.payload.action !== 'opened') {
                 console.log('No issue or PR was opened, skipping');
                 return;
@@ -535,13 +550,8 @@ function run() {
                 console.log('The event that triggered this action was not a issue, skipping.');
                 return;
             }
-            const client = github.getOctokit(gitHubToken);
-            const { data: issue } = yield client.issues.get({
-                owner: github.context.repo.owner,
-                repo: github.context.repo.repo,
-                issue_number: github.context.issue.number
-            });
-            const response = yield pii.callPiiDetectionEndpoint(issue.body, url, subKey);
+            console.log("TEXT TO CHECK: " + textToCheck);
+            const response = yield pii.callPiiDetectionEndpoint(textToCheck, url, subKey);
             let containsPii = false;
             if (response) {
                 response.documents.forEach(doc => {
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,29 @@ async function run(): Promise<void> {
   try {
     const subKey = core.getInput("azureCognitiveSubscriptionKey", { required: true })
     const url = core.getInput("azureCognitiveEndpoint", { required: true })
+    const gitHubToken = core.getInput("gitHubToken", { required: true })
 
-    console.log(github.context.payload);
-    console.log(JSON.stringify(github.context.payload));
+    console.log(`PAYLOAD: ${github.context.payload}`);
+    console.log(`STRINGIFIED: ${JSON.stringify(github.context.payload)}`);
+
+    const client = github.getOctokit(gitHubToken);
+    let textToCheck;
+
+    if (github.context.payload.issue && (github.context.payload.action == 'opened' || github.context.payload.action == 'updated')) {
+      //An issue was opened or updated
+      const { data: issue } = await client.issues.get({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: github.context.issue.number
+      });
+
+      textToCheck = issue.body;
+    }
+
+    if (github.context.payload.comment && github.context.payload.action == 'created') {
+      //A comment was added to the issue
+      textToCheck = github.context.payload.comment.body;
+    }
 
     if (github.context.payload.action !== 'opened') {
       console.log('No issue or PR was opened, skipping');
@@ -20,15 +40,9 @@ async function run(): Promise<void> {
       return;
     }
 
-    const client = github.getOctokit(gitHubToken);
+    console.log("TEXT TO CHECK: " + textToCheck);
 
-    const { data: issue } = await client.issues.get({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      issue_number: github.context.issue.number
-    });
-
-    const response = await pii.callPiiDetectionEndpoint(issue.body, url, subKey)
+    const response = await pii.callPiiDetectionEndpoint(textToCheck, url, subKey)
     let containsPii = false;
 
     if (response) {
