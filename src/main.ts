dfade77818823274e541a3fc557064d3660d4df5
Rob DeRosa <rob-derosa@github.com>
2020-07-26 16:01:03 -0600

    based on issues now

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,54 @@
 import * as pii from "./pii-detector";
 import * as core from "@actions/core";
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const subKey = core.getInput("azureCognitiveSubscriptionKey")
-    const url = core.getInput("azureCognitiveEndpoint")
-    const text = core.getInput("textToCheck")
-    const response = await pii.callPiiDetectionEndpoint(text, url, subKey)
+    const subKey = core.getInput("azureCognitiveSubscriptionKey", { required: true })
+    const url = core.getInput("azureCognitiveEndpoint", { required: true })
+
+    console.log(github.context.payload);
+    console.log(JSON.stringify(github.context.payload));
+
+    if (github.context.payload.action !== 'opened') {
+      console.log('No issue or PR was opened, skipping');
+      return;
+    }
+
+    if (!github.context.payload.issue) {
+      console.log('The event that triggered this action was not a issue, skipping.');
+      return;
+    }
+
+    const client = github.getOctokit(gitHubToken);
+
+    const { data: issue } = await client.issues.get({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: github.context.issue.number
+    });
+
+    const response = await pii.callPiiDetectionEndpoint(issue.body, url, subKey)
+    let containsPii = false;
 
     if (response) {
       response.documents.forEach(doc => {
         doc.entities.forEach(ent => {
           console.log(`${ent.confidenceScore} : ${ent.category} - ${ent.text}`)
+          containsPii = true;
         });
       });
       core.setOutput("results", JSON.stringify(response));
+
+      if (containsPii) {
+        let labels = ["PII"];
+        client.issues.addLabels({
+          labels: labels,
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          issue_number: github.context.issue.number
+        })
+      }
     }
   } catch (error) {
     console.log(error);
