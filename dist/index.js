68dcf116defcf678aec72e09012c726240c4d7f7
Rob DeRosa <rob-derosa@github.com>
2020-07-26 16:54:25 -0600

    updates

--- dist/index.js
+++ dist/index.js
@@ -528,35 +528,30 @@ function run() {
             console.log(github.context.payload);
             const client = github.getOctokit(gitHubToken);
             let textToCheck;
+            let containsPii = false;
+            let issueNumber;
             if (github.context.payload.issue && (github.context.payload.action === 'opened' || github.context.payload.action === 'edited')) {
                 //An issue was opened or updated
-                const { data: issue } = yield client.issues.get({
-                    owner: github.context.repo.owner,
-                    repo: github.context.repo.repo,
-                    issue_number: github.context.issue.number
-                });
-                textToCheck = issue.body;
+                textToCheck = github.context.payload.issue.body;
+                issueNumber = github.context.issue.number;
             }
             if (github.context.payload.comment && (github.context.payload.action === 'created' || github.context.payload.action === 'edited')) {
                 //A comment was added to the issue
                 textToCheck = github.context.payload.comment.body;
+                issueNumber = github.context.issue.number;
             }
             if (github.context.payload.pull_request && (github.context.payload.action === 'opened' || github.context.payload.action === 'edited')) {
-                //An issue was opened or updated
-                // const { data: issue } = await client.p.get({
-                //   owner: github.context.repo.owner,
-                //   repo: github.context.repo.repo,
-                //   issue_number: github.context.issue.number
-                // });
-                // textToCheck = issue.body;
+                //A pull request was opened or updated
+                textToCheck = github.context.payload.pull_request.body;
+                issueNumber = github.context.payload.pull_request.number;
             }
             const response = yield pii.callPiiDetectionEndpoint(textToCheck, url, subKey);
-            let containsPii = false;
             if (response) {
                 console.log("\n\n------------------------------------------------------");
                 response.documents.forEach(doc => {
                     doc.entities.forEach(ent => {
                         let log = `${ent.category} detected with ${ent.confidenceScore * 100}% confidence score and a value of: '${ent.text}'`;
+                        //We only care about results with a confidence score of 60% or higher
                         if (ent.confidenceScore >= .6) {
                             containsPii = true;
                         }
@@ -573,7 +568,7 @@ function run() {
                         labels: labels,
                         owner: github.context.repo.owner,
                         repo: github.context.repo.repo,
-                        issue_number: github.context.issue.number
+                        issue_number: issueNumber
                     });
                 }
                 else {
--- src/main.ts
+++ src/main.ts
@@ -12,36 +12,28 @@ async function run(): Promise<void> {
 
     const client = github.getOctokit(gitHubToken);
     let textToCheck;
+    let containsPii = false;
+    let issueNumber;
 
     if (github.context.payload.issue && (github.context.payload.action === 'opened' || github.context.payload.action === 'edited')) {
       //An issue was opened or updated
-      const { data: issue } = await client.issues.get({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        issue_number: github.context.issue.number
-      });
-
-      textToCheck = issue.body;
+      textToCheck = github.context.payload.issue.body;
+      issueNumber = github.context.issue.number;
     }
 
     if (github.context.payload.comment && (github.context.payload.action === 'created' || github.context.payload.action === 'edited')) {
       //A comment was added to the issue
       textToCheck = github.context.payload.comment.body;
+      issueNumber = github.context.issue.number;
     }
 
     if (github.context.payload.pull_request && (github.context.payload.action === 'opened' || github.context.payload.action === 'edited')) {
-      //An issue was opened or updated
-      // const { data: issue } = await client.p.get({
-      //   owner: github.context.repo.owner,
-      //   repo: github.context.repo.repo,
-      //   issue_number: github.context.issue.number
-      // });
-
-      // textToCheck = issue.body;
+      //A pull request was opened or updated
+      textToCheck = github.context.payload.pull_request.body;
+      issueNumber = github.context.payload.pull_request.number;
     }
 
     const response = await pii.callPiiDetectionEndpoint(textToCheck, url, subKey)
-    let containsPii = false;
 
     if (response) {
       console.log("\n\n------------------------------------------------------");
@@ -50,6 +42,7 @@ async function run(): Promise<void> {
 
           let log = `${ent.category} detected with ${ent.confidenceScore * 100}% confidence score and a value of: '${ent.text}'`
 
+          //We only care about results with a confidence score of 60% or higher
           if(ent.confidenceScore >= .6)
           {
             containsPii = true;
@@ -63,11 +56,12 @@ async function run(): Promise<void> {
 
       if (containsPii) {
         let labels = ["PII"];
+
         client.issues.addLabels({
           labels: labels,
           owner: github.context.repo.owner,
           repo: github.context.repo.repo,
-          issue_number: github.context.issue.number
+          issue_number: issueNumber
         })
       } else {
         console.log(`No PII detected in:\n${textToCheck}`)
